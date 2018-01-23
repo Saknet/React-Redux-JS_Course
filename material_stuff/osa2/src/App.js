a8dcf58fb19702b5db38603bb22ca8408c7e3794
Psion <andreas.andra@helsinki.fi>
2018-01-24 00:43:52 +0200

    matskua eteenpäin

--- material_stuff/osa2/src/App.js
+++ material_stuff/osa2/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Note from './components/Note'
-import axios from 'axios'
+import noteService from './services/notes'
 
 class App extends React.Component {
   constructor(props) {
@@ -14,31 +14,30 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    console.log('will mount')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        this.setState({ notes: response.data })
-      })
+    noteService
+    .getAll()
+    .then(notes => {
+      this.setState({notes})
+    })
   }
 
   addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: this.state.newNote,
-      date: new Date().new,
-      important: Math.random() > 0.5,
-      id: this.state.notes.length + 1
+      date: new Date(),
+      important: Math.random() > 0.5
     }
   
-    const notes = this.state.notes.concat(noteObject)
-  
-    this.setState({
-      notes,
-      newNote: ''
-    })
-  }
+    noteService
+      .create(noteObject)
+      .then(newNote => {
+        this.setState({
+          notes: this.state.notes.concat(newNote),
+          newNote: ''
+        })
+      })
+    }
 
   handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -47,6 +46,26 @@ class App extends React.Component {
 
   toggleVisible = () => {
     this.setState({showAll: !this.state.showAll})
+  }
+
+  toggleImportanceOf = (id) => {
+    return () => {
+      const note = this.state.notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+  
+      noteService
+        .update(id, changedNote)
+        .then(changedNote => {
+          const notes = this.state.notes.filter(n => n.id !== id)
+          this.setState({
+            notes: notes.concat(changedNote)
+          })
+        })
+        .catch(error => {
+          alert(`muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`)
+          this.setState({ notes: this.state.notes.filter(n => n.id !== id) })
+        })
+    }
   }
 
   render() {
@@ -66,9 +85,15 @@ class App extends React.Component {
             näytä {label}
           </button>
         </div>
-
+        
         <ul>
-          {notesToShow.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note =>
+        <Note
+        key={note.id}
+        note={note}
+        toggleImportance={this.toggleImportanceOf(note.id)}
+        />
+        )}
         </ul>
         <form onSubmit={this.addNote}>
           <input
--- material_stuff/osa2/src/index.js
+++ material_stuff/osa2/src/index.js
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 
-import axios from 'axios'
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
